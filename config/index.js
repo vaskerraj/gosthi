665534035fdd0be0b381d093e3beb9f869921e5f
vaskerraj <yohungdha@gmail.com>
2020-07-10 15:37:58 +0545

    add smtp email info as module

--- config/index.js
+++ config/index.js
@@ -2,5 +2,9 @@ require("dotenv").config();
 
 module.exports = {
     PORT : process.env.APP_PORT,
-    SECRET : process.env.APP_SECRET
+    SECRET : process.env.APP_SECRET,
+    EMAIL_SMTP_ADD : process.env.EMAIL_SMTP_ADD,
+    EMAIL_SMTP_PORT : process.env.EMAIL_SMTP_PORT,
+    EMAIL_SMTP_USER : process.env.EMAIL_SMTP_USER,
+    EMAIL_SMTP_PASS : process.env.EMAIL_SMTP_PASS,
 }
