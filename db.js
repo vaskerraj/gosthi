47e9e366012beaea50035d39e0360cc05ba735b9
vaskerraj <yohungdha@gmail.com>
2020-07-11 23:26:14 +0545

    rearrange mysql info and get from .env

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE } = require('./config/index');
 const mysql = require('mysql');
 
 var pool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'gosthi',
+    host : MYSQL_HOST,
+    user : MYSQL_USER,
+    password : MYSQL_PASS,
+    database : MYSQL_DATABASE,
     connectionLimit : 1000
 });
 
