46c74cb598dbb71470c3fbac3a4a6fd4c897a05e
vaskerraj <yohungdha@gmail.com>
2020-07-13 19:16:13 +0545

    add mysql information

--- config/index.js
+++ config/index.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 module.exports = {
     PORT : process.env.APP_PORT,
     SECRET : process.env.APP_SECRET,
+
+    MYSQL_HOST : process.env.MYSQL_HOST,
+    MYSQL_USER : process.env.MYSQL_USER,
+    MYSQL_PASS : process.env.MYSQL_PASS,
+    MYSQL_DATABASE : process.env.MYSQL_DATABASE,
+
     EMAIL_SMTP_ADD : process.env.EMAIL_SMTP_ADD,
     EMAIL_SMTP_PORT : process.env.EMAIL_SMTP_PORT,
     EMAIL_SMTP_USER : process.env.EMAIL_SMTP_USER,
